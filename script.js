9d3b09faa011dea57f839383aff80996e84ae450
Lucas Nakahara <nakaharalucas2@gmail.com>
2025-03-16 00:34:04 -0300

    vamos ver 2.0

--- script.js
+++ script.js
@@ -33,3 +33,23 @@ document.querySelectorAll('.project-card').forEach(card => {
     });
 });
 
+
+
+
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {  
+        scrollToTopButton.classList.remove("hidden");
+    } else {
+        scrollToTopButton.classList.add("hidden");
+    }
+});
+
+scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
